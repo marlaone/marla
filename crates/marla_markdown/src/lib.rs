cb72a2075ca466bf7cb802941083bc6c402246a4
MrParano1d <jps@marla.one>
2022-08-19 17:04:56 +0200

    fix(marla_markdown): :bug: markdown SoftBreak -> HardBreak

--- crates/marla_markdown/src/lib.rs
+++ crates/marla_markdown/src/lib.rs
@@ -3,13 +3,16 @@ use std::path::PathBuf;
 use ammonia::Builder;
 use anyhow::Result;
 use maplit::hashset;
-use pulldown_cmark::{html, Options, Parser};
+use pulldown_cmark::{html, Event, Options, Parser};
 
 pub mod frontmatter;
 
 pub fn markdown_to_html(markdown_input: &str) -> Result<String> {
     let markdown_options = Options::all();
-    let parser = Parser::new_ext(markdown_input, markdown_options);
+    let parser = Parser::new_ext(markdown_input, markdown_options).map(|event| match event {
+        Event::SoftBreak => Event::HardBreak,
+        _ => event,
+    });
 
     let mut html_output = String::with_capacity(markdown_input.len() * 3 / 2);
     html::push_html(&mut html_output, parser);
