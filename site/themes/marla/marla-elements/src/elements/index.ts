6d3e75207e1ac812af83ed1eb5d22009e45a7d85
MrParano1d <jps@marla.one>
2022-08-22 08:59:57 +0200

    fix(theme_marla): :bug: fixed missing href for mobile link

--- site/themes/marla/marla-elements/src/elements/index.ts
+++ site/themes/marla/marla-elements/src/elements/index.ts
@@ -1,11 +1,14 @@
-import { FlowComponent, JSX } from 'solid-js'
-import { Slots } from '../utils'
+import { FlowComponent, JSX } from "solid-js";
+import { Slots } from "../utils";
 
 export type MarlaProps<P> = {
-  slots: Slots
-  props: P
-}
+  slots: Slots;
+  props: P;
+};
 
-export type MarlaElement<P> = FlowComponent<MarlaProps<P>, Element[]|JSX.Element>
+export type MarlaElement<P> = FlowComponent<
+  MarlaProps<P>,
+  Element[] | JSX.Element
+>;
 
-export * from './hello-world'
+export * from "./hello-world";
